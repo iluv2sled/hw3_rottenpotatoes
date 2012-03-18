562212503e02396f7efb766754609dddaa0abd13
Todd Crook <todd.crook@gmail.com>
2012-03-18 19:18:20 +0000

    add tar ball

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -4,8 +4,8 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    Movie.create!(movie)
   end
-  assert false, "Unimplmemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -14,7 +14,9 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.content  is the entire content of the page as a string.
-  assert false, "Unimplmemented"
+  puts "i am here #{e1.inspect}, #{e2.inspect}"
+  
+  assert e1 < e2, "not in sorted order"
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -26,3 +28,16 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
 end
+
+#Given /I have added "(.*)" with rating "(.*)"/ do |title, rating|
+#  Given %Q{I am on the Create New Movie page}
+#  When  %Q{I fill in "Title" with "#{title}"}
+#  And   %Q{I select "#{rating}" from "Rating"}
+#  And   %Q{I press "Save Changes"}
+#end
+
+#Then /I should see "(.*)" before "(.*)" on (.*)/ do |string1, string2, path|
+#  Given %Q{I am on #{path}}
+#  regexp = Regexp.new ".*#{string1}.*#{string2}"
+#  page.body.should =~ regexp
+#end
